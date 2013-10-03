6c056b3da042bd2f83e01f76796d2e3c93cc1c2f
Rachel Werner <rachelannwerner@gmail.com>
2013-10-03 11:14:21 -0500

    photoshop lite

--- 2013-10-02-Photoshop-Lite/js/main.js
+++ 2013-10-02-Photoshop-Lite/js/main.js
@@ -1,17 +1,59 @@
+'use strict';
+
 $(document).ready(initialize);
 
 function initialize()
 {
   $('#add_color').click(addColor);
+  $('#brush').click(brushClicked);
+  //$('.box').click(boxClicked);
+  // Making an event happen before the object gets created.
+  //$('parent_selector').on('name of event', 'child selector', nameOfFuntion)
+  $('#colors').on('click', '.box', boxClicked);
+  $('#add_box').click(addLittleBoxClicked);
+  $('#boxes').on('mouseover', '.littlebox', boxHover);
 }
+
+function boxHover()
+{
+  var $littlebox = $(this);
+  var brushColor = $('#brush').css('background-color');
+  $littlebox.css('background-color', brushColor);
+
+}
+
+function addLittleBoxClicked()
+{
+  var amount = $('#amount').val();
+  amount = parseInt(amount, 10);
+  for(var i = 0; i < amount; i++)
+  {
+    var $div = $('<div>');
+    $div.addClass('littlebox');
+
+    $('#boxes').append($div);
+  }
+}
+
+function brushClicked()
+{
+  alert('the brush was clicked');
+}
+
+function boxClicked()
+{
+  var $box = $(this); //converts a div into a jquery obeject
+  var color = $box.css('background-color'); //what is the background color of the box you clicked
+  $('#brush').css('background-color', color);
+}
+
 
 function addColor()
 {
-  debugger;
   var color = $('#color').val();
   var $div = $('<div>');
   $div.addClass('box');
-  $div.css('background-color', 'color');
+  $div.css('background-color', color);
 
   $('#colors').prepend($div);
   clearInputAndFocus();
